c56df45f8fe0e12ddf0bcbab933ff1dc4384c9bc
Jie CAO <lilahkmars@gmail.com>
2026-08-13 13:56:45 +1200

    chore: create test console app for output

--- TaxCalculator.ConsoleApp/Program.cs
+++ TaxCalculator.ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using TaxCalculator;
+
+var brackets = DefaultTaxBrackets.Brackets();
+var calculator = new TaxCalculator.TaxCalculator(brackets);
+var generator = new PayslipGenerator(calculator);
+var formatter = new PayslipFormatter(CultureInfo.InvariantCulture);
+
+var employee = new Employee("Mary Song", 60000m);
+
+var payslip = generator.Generate(employee);
+Console.WriteLine(formatter.Format(payslip));
